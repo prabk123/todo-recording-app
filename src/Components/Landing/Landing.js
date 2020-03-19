890695ed75bf559d47a02b223a6ef9b8654e57d0
Prabodh Kakodkar <prabodhkakodkar@prabodhs-mbp.home>
2020-03-19 14:07:22 +0000

    add wave animation to the bottom of landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -21,6 +21,10 @@ const Landing = () => {
           Get Started
         </Button>
       </div>
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
     </div>
   );
 };
